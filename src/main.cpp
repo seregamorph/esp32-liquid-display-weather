37fb6560a9252047c72ea020265e7a56fc54c424
seregamorph <serega.morph@gmail.com>
2022-02-12 21:22:06 +0100

    header

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #include <LiquidCrystal_I2C.h>
 
 // set the LCD number of columns and rows
@@ -10,14 +9,14 @@ int lcdRows = 2;
 // if you don't know your display address, run an I2C scanner sketch
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-void setup(){
+void setup() {
     // initialize LCD
     lcd.init();
     // turn on LCD backlight
     lcd.backlight();
 }
 
-void loop(){
+void loop() {
     // set cursor to first column, first row
     lcd.setCursor(0, 0);
     // print message
@@ -26,7 +25,7 @@ void loop(){
     // clears the display to print new message
     lcd.clear();
     // set cursor to first column, second row
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("Muesenka!");
     delay(1000);
     lcd.clear();
